4290e475c038a13e951d25dd7c083906aa776c74
Thomas MacLean <thomas.maclean@gmail.com>
2022-06-09 23:11:51 +0200

    add csv file

---
+++ jokes.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const jokes = fs.readFileSync(__dirname + "/jokes.csv", "utf8").split("\n");
+
+const randomJoke = () => {
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  return jokes[randomIndex].split(',"')[1].slice(0, -1).split('""').join('"');
+};
+
+console.log(randomJoke());
+module.exports = {
+  randomJoke,
+};
